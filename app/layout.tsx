a477d17121c92ab517b555db6c95d1c398097a1c
rashad <rashadhusein440@gmail.com>
2026-02-16 22:53:18 +0200

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import SmoothScroll from "@/components/SmoothScroll";
 import { Toaster } from "sonner";
 
 const geistSans = Geist({
@@ -16,8 +15,17 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Portfolio",
+  title: "Rashad Hussein | Web Developer",
   description: "Portfolio of a software engineer",
+  keywords: ["Rashad Hussein", "Web Developer", "Software Engineer"],
+  authors: [{ name: "Rashad Hussein" }],
+  openGraph: {
+    title: "Rashad Hussein | Web Developer",
+    description: "Portfolio of a software engineer",
+    type: "website",
+    locale: "en_US",
+    siteName: "Rashad Hussein | Web Developer",
+  },
 };
 
 export default function RootLayout({
@@ -27,6 +35,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#0f172a" />
+        <meta name="description" content="Portfolio of a software engineer" />
+        <meta
+          name="keywords"
+          content="Rashad Hussein, Web Developer, Software Engineer"
+        />
+        <meta name="author" content="Rashad Hussein" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -39,7 +58,6 @@ export default function RootLayout({
           {children}
           <Toaster position="top-center" richColors />
         </ThemeProvider>
-        {/* <SmoothScroll /> */}
       </body>
     </html>
   );
